7fd774b4c57bcfe67d5679d705be6da394164091
jmponce80@gmail.com <jponce@Jose.local>
2015-05-26 11:43:28 +0200

    store

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -1,4 +1,4 @@
-
+#---
 # Excerpted from "Agile Web Development with Rails",
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material, 
@@ -6,16 +6,16 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-class Product < ActiveRecord::Base
-  validates :title, :description, :image_url, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+require 'test_helper'
 
-  validates :title, uniqueness: true
-  validates :image_url, allow_blank: true, format: {
-    with:    %r{\.(gif|jpg|png)\Z}i,
-    message: 'must be a URL for GIF, JPG or PNG image.'
-  }
-  def self.latest
-  	Product.order(:updated_at).last
+class StoreControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
- end
+
+end
